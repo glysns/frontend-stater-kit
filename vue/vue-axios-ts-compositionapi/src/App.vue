c6379aff807c96ae35831b3102340ce29cf4f597
Gleyson Sampaio <gleyson@digytal.com.br>
2023-04-21 22:31:05 -0300

    ts

--- vue/vue-axios-ts-compositionapi/src/App.vue
+++ vue/vue-axios-ts-compositionapi/src/App.vue
@@ -1,68 +1,15 @@
-<template>
-  <div>
-    <button @click='listar()'>Listar Clientes</button>
-    <button @click='buscar()'>Buscar Cliente -> ID</button>
-    <input type='text' placeholder='ID' size='1' v-model='id'/>
-    <button @click='alterar()'>Alterar ID 1</button>
-    <button @click='incluir()'>Incluir Novo</button>
-    <button @click='excluir()'>Excluir</button>
-    <input type='text' placeholder='ID' size='1' v-model='id'/>
-    <h4>Resposta</h4>
-    {{  resposta }}
-  </div>
+<script lang="ts">
+import { defineComponent } from 'vue'
 
-</template>
-
-<script>
-
-
-export default {
-  name: 'App',
-  data(){
+export default defineComponent({
+  data() {
     return {
-      id:0,
-      resposta:'SEM RESPOSTA'
-    } 
-  },
-  methods:{
-    async listar(){
-      console.log('listando ... ')
-      
-      console.log('listagem concluida ')
-    },
-    async buscar(){
-      console.log('buscando ... ')
-      
-      console.log('busca concluida ... ')
-    },
-    async alterar(){
-      //const registro= { 'nome': 'gleyson sampaio de oliveira', 'cpf': '897870', 'dataNascimento': '1982-01-01', 'rendaMensal': 1234.5, 'sexo': 'MASCULINO', 'id': 1 }
-      console.log('alterando ... ')
-      
-      console.log('alteração concluida ... ')
-    },
-    async incluir(){
-      //const registro= { 'nome': 'marilene sampaio', 'cpf': '4564567', 'dataNascimento': '1967-04-01', 'rendaMensal': 9097.5, 'sexo': 'FEMININO'}
-      console.log('incluindo ... ')
-      
-      console.log('inclusão concluida ... ')
-    },
-    async excluir(){
-      console.log('excluindo ... ')
-      
-      console.log('exclusão concluida ... ')
+      count: 1
     }
   }
-}
+})
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+ <div>ola</div>
+</template>
---
+++ vue/vue-axios-typescript/README.md
@@ -0,0 +1,51 @@
+# Digytal - Programação, Pesquisa e Educação
+[www.digytal.com.br](http://www.digytal.com.br)
+
+[(11) 95894-0362](https://api.whatsapp.com/send?phone=5511958940362)
+
+#### Autores
+- [Gleyson Sampaio](https://github.com/glysns)
+
+## FrontEnd - StarterKit - Vue JS - Axios - TS - Composition API
+
+Este projeto foi criado através do projeto [Vue3 - Axios](https://github.com/glysns/frontend-stater-kit/tree/main/vue/vue-axios) para explorar exemplos de como consumir uma API Rest utilizando Axios e sendo implementado seguindo os padrões do TypeScript e Composition API.
+### Instalando dependências
+
+```shell
+npm install
+```
+
+### Instalando o Axios
+
+Axios é umas das alternativas para se realizar requisições HTTP.
+
+```shell
+npm install axios
+
+```
+
+### Inicie a aplicação
+
+```shell
+npm run serve
+```
+
+### Abre o seu navegador e cole a url abaixo
+
+```shell
+http://localhost:8080/
+```
+
+![image](https://github.com/glysns/frontend-stater-kit/blob/main/img/vue.png)
+
+### Compilação para produção
+
+```shell
+npm run build
+```
+
+### Analise da aplicação
+
+```shell
+npm run lint
+```
