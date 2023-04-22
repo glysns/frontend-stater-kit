c1e1019ea662c813cef1ece73af467a843091912
Gleyson Sampaio <gleyson@digytal.com.br>
2023-04-21 23:29:20 -0300

    crud

--- vue/vue-axios-typescript/src/http/cliente-resource.ts
+++ vue/vue-axios-typescript/src/http/cliente-resource.ts
@@ -6,19 +6,19 @@ class ClienteResource {
         return http.get(this.servico);
     }
 
-    get(id: any): Promise<any> {
+    buscar(id: any): Promise<any> {
         return http.get(`${this.servico}/${id}`);
     }
 
-    create(data: any): Promise<any> {
+    incluir(data: any): Promise<any> {
         return http.post(this.servico, data);
     }
 
-    update(id: any, data: any): Promise<any> {
+    alterar(id: any, data: any): Promise<any> {
         return http.put(`${this.servico}/${id}`, data);
     }
 
-    delete(id: any): Promise<any> {
+    excluir(id: any): Promise<any> {
         return http.delete(`${this.servico}/${id}`);
     }
 }
